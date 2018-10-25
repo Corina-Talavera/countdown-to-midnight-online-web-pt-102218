8804b971e01bc8273f4ef53ba25a34635096084e
Corina Talavera <corina.talavera@gmail.com>
2018-10-25 23:11:22 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,7 +6,7 @@ def countdown(seconds)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(5)
+def countdown_with_sleep(seconds)
   while seconds > 0
     puts "#{seconds} SECOND(S)!"
     seconds -= 1
